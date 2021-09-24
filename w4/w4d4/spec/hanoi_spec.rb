1c6050a434f9fce0289c16679aae19e20c4b663e
Lyhour Lay <lyhourlay1@gmail.com>
2021-09-23 17:19:52 -0700

    add won? spec

--- w4/w4d4/spec/hanoi_spec.rb
+++ w4/w4d4/spec/hanoi_spec.rb
@@ -1,7 +1,7 @@
 require "hanoi"
 
 describe "Hanoi" do
-  subject(:h){Hanoi.new([4, 5, 1, 6, 3, 2])}
+  subject(:h){Hanoi.new([1,2,3,4,5,6])}
 
   describe "#intialize" do
     it "raise error if no argument is passed" do
@@ -9,30 +9,25 @@ describe "Hanoi" do
     end
 
     it "create array1 with passed in argument" do
-      expect(h.array1).to eq([4, 5, 1, 6, 3, 2])
+      expect(h.array1).to eq([1,2,3,4,5,6])
     end
 
     it "create two empty arrays" do
       expect(h.array2).to be_empty
       expect(h.array3).to be_empty
     end
+  end
+  describe "#won?" do 
+    it "move all elements to array3" do
+      expect(h.array3).to eq([1,2,3,4,5,6])
+      expect(h.array2).to be_empty
+      expect(h.array1).to be_empty
+    end
+  end
 
-
-
-    # it "creates three arrays to store values" do
-    #   expect(h.arrays.length).to eq(3)
-    # end
-
-    # it "creates three empty arrays" do
-    #   expect(h.arrays.all?{|el| el.empty?}).to be_truthy
-    # end
-
-    # it "creates unique arrays" do
-
-    # end
-
-
-
+  describe "#move" do 
+    it "promts user for get" do 
+    end
   end
 
 end
