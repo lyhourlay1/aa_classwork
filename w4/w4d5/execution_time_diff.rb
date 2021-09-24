7d9febaef4543706fb32786a317c5554637a2db7
Lyhour Lay <lyhourlay1@gmail.com>
2021-09-24 14:19:06 -0700

    add largest method

--- w4/w4d5/execution_time_diff.rb
+++ w4/w4d5/execution_time_diff.rb
@@ -1,14 +1,15 @@
 
 def my_min1(list)
-  list.each do |ele1|
-    list.each do |ele2|
+  list.each.with_index do |ele1,idx1|
+    list.each.with_index do |ele2,idx2|
       if ele1 > ele2
-        ele2 
+        list.delete_at(idx1)
       else
-        ele1 
+        list.delete_at(idx2)
       end 
     end
   end
+  list.min
 end
 
 
@@ -24,4 +25,23 @@ def my_min2(list)
 end
 
 list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
-p my_min1(list) #=> 
+# p my_min1(list) #=> 
+
+def largest_contiguous_subsum(list)
+  sub_arr=[list]
+  list.each.with_index do |el1, idx1|
+    sub_arr<< [el1]
+    list.each.with_index do |el2, idx2|
+      if idx1 != idx2 && !sub_arr.include?([el1,el2].sort)
+        sub_arr << [el1,el2].sort
+      end
+    end
+  end
+  #need to sum sub array and return max
+  sub_arr
+end
+
+p largest_contiguous_subsum([5,3,1,8]) #->[[5,3,1], [5,3],[5,1], [1, 3], [5], [1], [3]]
+[2]
+[2,3]
+[2],[2,3]
