1bf75af05af0e762d661d94ca04271b23347e047
William Winzeler <williamwinzeler@MacBook-Pro-3.local>
2021-09-24 16:38:00 -0700

    finished anagrams

--- w4/w4d5/anagram.rb
+++ w4/w4d5/anagram.rb
@@ -5,5 +5,48 @@ def anagram?(str1, str2)
     all_combos.include?(arr2)
 end
 
-# p anagram?("gizmo", "sally")    #=> false
-# p anagram?("elvis", "lives")    #=> true
+def anagram2?(str1, str2)
+  extra_indice = []
+  str2 = str2.split("")
+  str1.each_char do |ele|
+    if not (str2.index(ele) == nil)
+      str2.delete_at(str2.index(ele))
+    else
+      extra_indice += [ele] 
+    end 
+  end
+
+  str2.empty? and extra_indice.empty?
+end
+
+def anagram3?(str1, str2)
+  str1.split("").sort == str2.split("").sort
+end
+
+def anagram4?(str1, str2)
+  hash1 = Hash.new(0)
+  hash2 = Hash.new(0)
+  str1.each_char do |ele|
+    hash1[ele] += 1
+  end
+  str2.each_char do |ele|
+    hash2[ele] += 1
+  end
+
+  hash1 == hash2
+end
+
+def anagram5?(str1, str2)
+  hash = Hash.new(0)
+  str1.each_char do |ele|
+    hash[ele] += 1
+  end
+  str2.each_char do |ele|
+    hash[ele] -= 1
+  end
+
+  hash.values.all?{|ele| ele == 0}
+end
+
+p anagram5?("gizmo", "sally")    #=> false
+p anagram5?("elvis", "lives")    #=> true
