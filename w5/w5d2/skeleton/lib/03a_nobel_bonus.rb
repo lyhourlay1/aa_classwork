219f1d3e1c9a8f06742f239b494a290b6ae01e4b
Lyhour Lay <lyhourlay1@gmail.com>
2021-09-28 16:04:17 -0700

    add larger than russia

--- w5/w5d2/skeleton/lib/03a_nobel_bonus.rb
+++ w5/w5d2/skeleton/lib/03a_nobel_bonus.rb
@@ -11,5 +11,11 @@ require_relative './sqlzoo.rb'
 def physics_no_chemistry
   # In which years was the Physics prize awarded, but no Chemistry prize?
   execute(<<-SQL)
+    SELECT 
+      yr
+    FROM 
+      nobels
+    WHERE
+      subject = 'Physics' AND NOT subject='Chemistry';
   SQL
 end
