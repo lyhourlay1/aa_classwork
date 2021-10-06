828eb92a84301a5868a80205b12f68374ab3672b
Lyhour Lay <lyhourlay1@gmail.com>
2021-10-06 14:51:41 -0700

    Add get and post

--- w6/w6d3/First_Routes_and_Controller/config/routes.rb
+++ w6/w6d3/First_Routes_and_Controller/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #resources :users
+
+  get '/users', to: 'users#index',  as: 'users'
+  post '/users', to: 'users#create', as: 'users'
+  get '/users/new', to: 'users#new', as: 'new_user'
+  get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
+  
 end
