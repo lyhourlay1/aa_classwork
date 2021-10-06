61df0822c9fca89c25d3b619ded742b5b4bf2318
Chris <chris.cheasty3@gmail.com>
2021-10-06 15:21:44 -0700

    Complete Routers and create control methods

---
+++ w6/w6d3/First_Routes_and_Controller/app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+
+  def index
+    users = User.all
+    render json: users
+  end
+
+  def show
+    user = User.find(params[:id])
+    render json: user
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      redirect_to "/users/#{user.id}"
+    else
+      render json: user.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+      render json: user
+    else
+      render json: user.errors.full_messages, status: 422
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:fav_food)
+  end
+
+end
--- w6/w6d3/First_Routes_and_Controller/config/routes.rb
+++ w6/w6d3/First_Routes_and_Controller/config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #resources :users
+  resources :users
+  #difference between put and patch?
 
-  get '/users', to: 'users#index',  as: 'users'
-  post '/users', to: 'users#create', as: 'users'
-  get '/users/new', to: 'users#new', as: 'new_user'
-  get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
-  
+  # get '/users', to: 'users#index',  as: 'users'
+  # post '/users', to: 'users#create'
+  # get '/users/new', to: 'users#new', as: 'new_user'
+  # get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
+  # get '/users/:id', to: 'users#show', as: 'user'
+  # patch '/users/:id', to: 'users#update'
+  # delete '/users/:id', to: 'users#delete'
+
 end
