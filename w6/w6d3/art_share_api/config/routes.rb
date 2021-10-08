22ff043685911d2b0508033bbbef4a97d595c331
Chris <chris.cheasty3@gmail.com>
2021-10-07 17:05:30 -0700

    Create controller for artwork_shares

--- w6/w6d3/art_share_api/config/routes.rb
+++ w6/w6d3/art_share_api/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # resources :ArtworkShare, only: [:create, :destroy]
+  
 
   resources :users, except: [:new, :edit]
   resources :artworks, except: [:new, :edit]
+  resources :artwork_shares, only: [:create, :destroy]
 end
