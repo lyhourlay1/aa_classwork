8abc3f0d3d37fb89d8ecc9d68a9138a3773d76e9
Lyhour Lay <lyhourlay1@gmail.com>
2021-10-08 15:15:39 -0700

    Finish model validations

--- w6/w6d5/cat99/app/models/cat.rb
+++ w6/w6d5/cat99/app/models/cat.rb
@@ -2,16 +2,16 @@ require 'action_view'
 class Cat < ApplicationRecord
 
   ActionView::Helpers::DateHelper
-
+  COLOR = %w(orange black brown).freeze
   validates :birth_date, :color, :name, :sex, :description, presence: true
-  validates :color, inclusion: { in: ["Orange", "Black", "Brown"] }
+  validates :color, inclusion: COLOR
+  validates :sex, inclusion: %w(F M)
+
 
 
   def age
     now = Time.now.utc.to_date
     now.year - self.birth_date.year - ((now.month > self.birth_date.month || (now.month == self.birth_date.month && now.day >= self.birth_date.day)) ? 0 : 1)
   end
-
-  def color
 
 end
