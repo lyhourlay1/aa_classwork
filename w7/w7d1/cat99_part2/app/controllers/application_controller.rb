60df82e959314a240d1b852eeb753f0a722616a4
Lyhour Lay <lyhourlay1@gmail.com>
2021-10-11 15:08:20 -0700

    Add destroy

--- w7/w7d1/cat99_part2/app/controllers/application_controller.rb
+++ w7/w7d1/cat99_part2/app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+    helper_method :current_user
+    def login(user)
+        session[:session_token] = user.reset_session_token!
+    end
+
+    def current_user
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+
 end
--- w7/w7d1/cat99_part2/app/controllers/sessions_controller.rb
+++ w7/w7d1/cat99_part2/app/controllers/sessions_controller.rb
@@ -2,5 +2,22 @@ class SessionsController < ApplicationController
   def new
     render :new
   end
+
+  def create
+    @user = User.find_by_credentials(params[:user][:username] , params[:user][:password])
+      if @user
+          login(@user)
+          redirect_to cats_url 
+      else
+          render json: ["Invalid Username or Passord"]
+      end
+  end
+
+  def destroy
+    user = current_user
+    user.reset_session_token!
+    session[:session_token]=nil 
+    redirect_to new_sess
+  end
   
 end
