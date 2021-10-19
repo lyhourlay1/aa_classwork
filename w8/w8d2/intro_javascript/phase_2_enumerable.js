7344e762376d3661251e2eab3cc84533b1d90c76
Lyhour Lay <lyhourlay1@gmail.com>
2021-10-19 16:50:55 -0700

    finish phase 2

--- w8/w8d2/intro_javascript/phase_2_enumerable.js
+++ w8/w8d2/intro_javascript/phase_2_enumerable.js
@@ -3,8 +3,27 @@ let cb = function (num) {
 }
 
 Array.prototype.myEach= function(cb){
-
-   this.forEach(el=> console.log(cb(el)));
+  let el;
+  for(let i=0; i < this.length; i++){
+    el = this[i]
+    console.log(cb(el))
+  }
 }
 
 console.log([1,2,3,4].myEach(cb));
+
+Array.prototype.myMap = function(cb){
+  let result=[];
+  this.myEach(function(el){
+    result.push(cb(el))
+  })
+  return result
+  // this.myEach(el => {
+  //   result.push(cb(el))
+  // })
+
+  // this.myEach(el => result.push(cb(el)))
+}
+
+
+console.log([1, 2, 3, 4].myMap(cb));
