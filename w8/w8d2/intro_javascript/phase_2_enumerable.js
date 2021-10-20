10d6a8d16568681581d8b4ebb53674a2c2d1491c
Cal Brown <calsbrown4@gmail.com>
2021-10-19 17:29:14 -0700

    Phase Two

--- w8/w8d2/intro_javascript/phase_2_enumerable.js
+++ w8/w8d2/intro_javascript/phase_2_enumerable.js
@@ -5,7 +5,7 @@ let cb = function (num) {
 Array.prototype.myEach= function(cb){
   let el;
   for(let i=0; i < this.length; i++){
-    el = this[i]
+    el = this[i];
     console.log(cb(el))
   }
 }
@@ -17,13 +17,36 @@ Array.prototype.myMap = function(cb){
   this.myEach(function(el){
     result.push(cb(el))
   })
-  return result
-  // this.myEach(el => {
-  //   result.push(cb(el))
-  // })
-
-  // this.myEach(el => result.push(cb(el)))
+  return result;
 }
 
 
 console.log([1, 2, 3, 4].myMap(cb));
+
+
+Array.prototype.myReduce = function(cb,initialValue){
+  if (initialValue == null){
+    
+    let result=this[0];
+
+    this.slice(1).myEach(function (el) {
+      result = cb(result, el);
+    })
+    return result;
+  } else{
+    let result=initialValue;
+    this.myEach(function (el) {
+      result = cb(result, el);
+    })
+    return result;
+  }
+ 
+}
+console.log([1, 2, 3].myReduce(function (acc, el) {
+  return acc + el;
+})); // => 6
+
+// with initialValue
+console.log([1, 2, 3].myReduce(function (acc, el) {
+  return acc + el;
+}, 25));
