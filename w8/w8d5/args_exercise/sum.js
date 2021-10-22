ea8f408581433134a0040e4acd8ee64d1a157cfe
Lyhour Lay <lyhourlay1@gmail.com>
2021-10-22 14:18:33 -0700

    finished sum

---
+++ w8/w8d5/args_exercise/sum.js
@@ -0,0 +1,27 @@
+function sum(){
+    let args = Array.from(arguments)
+    let result = 0;
+    args.forEach(element => {
+        result+= element;
+    });
+    return result;
+}
+
+function sum(...arguments) {
+    let result = 0;
+    arguments.forEach(element => {
+        result += element;
+    });
+    return result;
+}
+
+// console.log(sum(1, 2, 3, 4));
+// console.log(sum(1, 2, 3, 4, 5));
+
+function myBind(cxt, bindArgs){
+    let func = this;
+    
+    return function(callArgs){
+        return func.call(cxt, callArgs);
+    }
+}
