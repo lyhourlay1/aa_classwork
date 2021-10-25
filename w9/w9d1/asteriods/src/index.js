de7558a24b253eb2c5d5c05e9a0b6d33caf467c5
ntademaru <ntademaru@gmail.com>
2021-10-25 10:52:17 -0700

    Setup for Asteroids

---
+++ w9/w9d1/asteriods/src/index.js
@@ -0,0 +1,6 @@
+const MovingObject = require("./moving_object.js");
+
+window.MovingObject = MovingObject;
+
+console.log("webpack is working!")
+console.log("test is working!")
---
+++ w9/w9d1/asteriods/src/moving_object.js
@@ -0,0 +1,10 @@
+
+function MovingObject(object) {
+  this.pos = object["pos"];
+  this.vel = object["vel"];
+  this.radius = object["radius"];
+  this.color = object["color"];
+};
+
+
+module.exports = MovingObject;
