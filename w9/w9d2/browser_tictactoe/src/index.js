ecf7225cf3ad4488837f5415eddabbe960b8fe1e
Lyhour Lay <lyhourlay1@gmail.com>
2021-10-26 14:31:17 -0700

    setting up the board

--- w9/w9d2/browser_tictactoe/src/index.js
+++ w9/w9d2/browser_tictactoe/src/index.js
@@ -1,6 +1,11 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require("./ttt-view.js")
+const Game = require("../ttt_node/game.js");
+
 
 document.addEventListener("DOMContentLoaded", () => {
   // Your code here
+  const game = new Game();
+  const viewElement = document.querySelector(".ttt")
+  const view = new View(game, viewElement )
+
 });
--- w9/w9d2/browser_tictactoe/src/ttt-view.js
+++ w9/w9d2/browser_tictactoe/src/ttt-view.js
@@ -1,7 +1,20 @@
 class View {
-  constructor(game, el) {}
+  constructor(game, el) {
+    this.game = game
+    this.element = el
+    this.setupBoard()
+  }
 
-  setupBoard() {}
+  setupBoard() {
+    this.element.innerHTML = `<ul></ul>`
+    let ul = this.element.querySelector("ul")
+    //ul.innerHTML = `<p> kdf</p>`
+    for(let i=1; i< 10; i++){
+      let li = document.createElement("li")
+      li.setAttribute("data-id", i)
+      ul.appendChild(li)
+    }
+  }
   
   bindEvents() {}
 
