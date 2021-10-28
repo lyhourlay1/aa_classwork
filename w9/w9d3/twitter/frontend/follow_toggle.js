cec2f4333cf0052aff79739fbf2445bf1f00bd60
Lyhour Lay <lyhourlay1@gmail.com>
2021-10-27 17:39:09 -0700

    rough day

--- w9/w9d3/twitter/frontend/follow_toggle.js
+++ w9/w9d3/twitter/frontend/follow_toggle.js
@@ -4,7 +4,8 @@ function FollowToggle(el) {
   this.userId = this.$el.data("userid");
   this.followState = this.$el.data("initial-follow-state");
   this.render();
-  this.$el.on("click", this.handleClick.bind(this));
+  this.$el.on("click",this.handleClick.bind(this));
+  //this.$el.on("click", this.render.bind(this))
 }
 
 FollowToggle.prototype.render = function() {
@@ -20,18 +21,23 @@ FollowToggle.prototype.handleClick = function(e) {
   //   $()
   // }
   e.preventDefault();
-  
   if (this.followState === "followed") {
     return $.ajax({
       method: 'DELETE',
-      url: `/users/${this.userid}/follow`
+      url: `/users/${this.userId}/follow`,
+      dataType: 'json'
+
     })
   } else {
+    console.log(this.$el)
+    this.$el.data("initial-follow-state", "followed")
     return $.ajax({
       method: 'POST',
-      url: `/users/${this.userid}/follow`
+      url: `/users/${this.userId}/follow`,
+      dataType: 'json'
     })
   }
+  this.render();
 }
 
 module.exports = FollowToggle;
