9ffe7f2b004768e8b77c72e660220fab4f176106
Lyhour Lay <lyhourlay1@gmail.com>
2021-11-16 16:25:39 -0800

    usercontrolelr

---
+++ w12/w12d2/benchBNB/app/models/user.rb
@@ -0,0 +1,56 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  email           :string           not null
+#  password_digest :string
+#  session_token   :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+
+    attr_reader :password # validate password length without storing in the database 
+
+    validates :username, presence: true, uniqueness:true
+    validates :password_digest, :session_token, presence: true
+    validates :password, length: {minimum: 6}, allow_nil: true
+
+    after_initialize :ensure_session_token
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        return nil unless user
+        if user.is_password?(password)
+            return user
+        else
+            return nil
+        end
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+    # Use BCrypt's built-in method for checking if the password provided is the user's password
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def ensure_session_token
+    # Generate the initial session_token so that we pass the validation
+    # This method runs right after the model is initialized, before any validations are run
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+
+  def reset_session_token!
+    # When a user logs out, we want to scramble their session_token so that bad people cannot use the old one
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save
+    self.session_token
+  end
+
+end
--- w12/w12d2/benchBNB/frontend/bench_bnb.jsx
+++ w12/w12d2/benchBNB/frontend/bench_bnb.jsx
@@ -1,1 +1,10 @@
 //npm install webpack webpack - cli react react - dom react - router - dom redux react - redux @babel/core @babel/preset - react @babel/preset-env babel-loader
+// frontend/bench_bnb.jsx
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const root = document.getElementById("root");
+    ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
+});
